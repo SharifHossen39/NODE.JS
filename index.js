40487ba1494d6f0d28a968f449bd28e33759cf51
SharifHossen <sharifhossen317@gmail.com>
2023-06-22 19:27:12 +0600

    first commit

--- index.js
+++ index.js
@@ -1,12 +1,34 @@
-const temp = require("./people");
-const lodash = require("lodash");
+/*
 
-temp.num += 1;
+title: uptime monitoring Application
+Description: A RESTful API to monitor up or down time of user defined links.
 
-console.log(temp.player);
-console.log(temp.num);
-temp.sharif();
+*/
 
-const last = lodash.last(temp.player);
-console.log(last);
+// dependencies
+const http = require("http");
+const { handleReqRes } = require("./helpers/handleReqRes");
+const enviroment = require("./helpers/enviroments.js");
+const data = require("./lib/data.js");
 
+// app object = module scaffolding
+const app = {};
+
+//@todo: pore muche dibo.
+// data.delete("test", "newFile", (err) => {
+//   console.log(err);
+// });
+
+// create server
+app.createServer = () => {
+  const server = http.createServer(app.handleReqRes);
+  server.listen(enviroment.port, () => {
+    console.log(`listneing to port number: ${enviroment.port}`);
+  });
+};
+
+//handle request and response
+app.handleReqRes = handleReqRes;
+
+//start the server
+app.createServer();
